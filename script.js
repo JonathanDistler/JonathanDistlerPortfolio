13649371848440d015d1628ff88726df7910b503
JonathanDistler <149265757+JonathanDistler@users.noreply.github.com>
2026-04-25 11:36:45 -0400

    Update script.js
    adds a new asset to Hyperloop with SQ25 aeroshell paper

--- script.js
+++ script.js
@@ -545,7 +545,8 @@ const projectData = {
             
         `,
         links: [
-            { text: "Open Hyperloop PDF", url: "assets/resume/Hyperloop_FA_25.pdf", type: "primary" }
+            { text: "Open Hyperloop FA-25 Report PDF", url: "assets/resume/Hyperloop_FA_25.pdf", type: "primary" }
+            { text: "Open Hyperloop SP-26 Aeroshell Technical Report PDF", url: "assets/resume/SQ5_Aero_Paper.pdf", type: "primary" }
         ]
     },
     4: {
